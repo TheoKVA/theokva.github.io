63787109d2eb8a92ebdb20d6b22671e7da9584c3
TheoKVA <apinto856@gmail.com>
2021-02-07 17:30:21 +0100

    Commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,28 +1,14 @@
 
+// STRUCTURE DU CODE BLE
 
+var characteristic, characteristic2; // Déclare les variables à l'extérieur pour y accéder
 
-
-const connectToDeviceAndSubscribeToUpdates = async () => {
+const connectToDeviceBLE = async () => {
 const device = await navigator.bluetooth
 	    .requestDevice({
 	    		acceptAllDevices: true,
 	    		optionalServices: ['00001234-0000-1000-8000-00805f9b34fb']
 	    	})
-	 //    .then(device => {
-  //           	console.log(device);
-  //           	device.gatt.connect();
-  //           	return this.device
-  //           })
-		// .then(server => {
-		// 	server.getPrimaryService('00001234-0000-1000-8000-00805f9b34fb');
-		// })
-		// .then(service => {
-		// 	  chosenHeartRateService = service;
-		// 	  return Promise.all([
-		// 	    service.getCharacteristic('00001234-0001-1000-8000-00805f9b34fb')
-		// 	      .then(logWorld),
-		// 	  ])
-		// })
         .catch(error => { console.error(error); });
 console.log(device);
 
@@ -32,35 +18,33 @@ console.log(server);
 const service = await server.getPrimaryService('00001234-0000-1000-8000-00805f9b34fb');
 console.log(service);
 
-const characteristic = await service.getCharacteristic('00001234-0001-1000-8000-00805f9b34fb');
+characteristic = await service.getCharacteristic('00001234-0001-1000-8000-00805f9b34fb');
 console.log(characteristic);
 
 characteristic.startNotifications();
 characteristic.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged);
-
 const reading = await characteristic.readValue();
 console.log("value = " + reading.getUint8(0));
 
 document.getElementById('informationsGet').innerHTML = reading.getUint8(0);
 
-// const characteristic = await 
-// 		service.getCharacteristic('19B10001-E8F2-537E-4F6C-D104768A1214');
-
-// 	characteristic.startNotifications();
-//    	characteristic.addEventListener('characteristicvaluechanged', 
-//    			handleCharacteristicValueChanged);
-
-// const reading = await 
-// 		characteristic.readValue();
-
-// console.log(reading.getUint8(0));
-
+characteristic2 = await service.getCharacteristic('00001234-0002-1000-8000-00805f9b34fb');
+console.log(characteristic2);
 
 };
+
 
 const handleCharacteristicValueChanged = (event) => {
 	console.log("new value was set = " + event.target.value.getUint8(0));
 	document.getElementById('informationsGet').innerHTML = event.target.value.getUint8(0);
 };
+
+
+function setCharacteristic(inputToPut) {
+	const buffer = new Uint8Array(2);
+	buffer[0] = inputToPut;
+	characteristic2.writeValue(buffer);
+};
+
 
 
